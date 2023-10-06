2bd0306c98f761e1a3b6f8c072fb114aff10b407
maryamalsaggfa <maryam1xsa@gmail.com>
2023-10-06 14:28:51 -0700

    Initial Commit

---
+++ CH1_MaryamV/CH1_MaryamVApp.swift
@@ -0,0 +1,17 @@
+//
+//  CH1_MaryamVApp.swift
+//  CH1_MaryamV
+//
+//  Created by maryam on 21/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct CH1_MaryamVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
